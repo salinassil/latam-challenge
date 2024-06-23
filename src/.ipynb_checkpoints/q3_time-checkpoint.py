e5d57eb933297ae2fd7e452212961e6d8116efeb
Silvia Salinas Ayaviri <silviasalinas@MacBook-Pro-de-Silvia.local>
2024-06-22 20:41:11 -0400

    agregando solución inicial

--- src/.ipynb_checkpoints/q3_time-checkpoint.py
+++ src/.ipynb_checkpoints/q3_time-checkpoint.py
@@ -1,19 +1,18 @@
-from typing import List, Tuple
 import json
 from collections import Counter
-import re
+from typing import List, Tuple
 
-#Top 10 usuarios más influyentes en función de las menciones (@)
 def q3_time(file_path: str) -> List[Tuple[str, int]]:
     mention_counter = Counter()
     
-    with open(file_path, 'r') as f:
+    with open(file_path, 'r', encoding='utf-8') as f:
         for line in f:
-       
+            try:
                 tweet = json.loads(line)
                 mentions = [mention['username'] for mention in tweet.get('mentionedUsers', [])]
                 mention_counter.update(mentions)
-       
+            except (json.JSONDecodeError, KeyError, TypeError) as e:
+                continue  # Salta líneas malformadas o con claves faltantes
     
-    
+  
     return mention_counter.most_common(10)
